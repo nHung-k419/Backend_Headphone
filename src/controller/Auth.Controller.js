3e066a189500283d92cb33d0f97a6e023c1258a4
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-08-11 23:39:15 +0700

    Update Auth.Controller.js

--- src/controller/Auth.Controller.js
+++ src/controller/Auth.Controller.js
@@ -47,19 +47,19 @@ const Login = async (req, res) => {
       httpOnly: true,
       secure: false,
       //   path: '/api/auth/refresh_token',
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      sameSite: "Strict",
+      sameSite: "none",
       secure: false, // Bắt buộc dùng ở production (HTTPS)
       maxAge: 15 * 60 * 1000, // 15 phút
     });
     res.cookie("User", JSON.stringify({ id: isCheckUser._id, Email: isCheckUser.Email, Name: isCheckUser.Name, Role: isCheckUser.Role }), {
       // httpOnly: true, // JS client không đọc được
       secure: true, // chỉ gửi qua HTTPS
-      sameSite: "Strict",
+      sameSite: "none",
       // maxAge: 60 * 60 * 1000, // 1h
     });
     return res.status(200).json({ message: "Login successfully", Email: isCheckUser.Email, Name: isCheckUser.Name, id: isCheckUser._id });
