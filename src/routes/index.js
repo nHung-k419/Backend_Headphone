c05c1c41dc013f2e6f346b59a55cc9069a02a8dc
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-08-19 21:30:13 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -57,38 +57,38 @@ import Decentralization from "../middleWare/Decentralization.js";
 import { getDistricts, getProvinces, getWards } from "../controller/Location.Controller.js";
 const router = express.Router();
 // Product Route
-router.post("/CreateProduct", VerifyAuth, Decentralization(["admin"]), uploadCloud.single("ImageUrl"), createProduct);
+router.post("/CreateProduct", Decentralization(["admin"]), uploadCloud.single("ImageUrl"), createProduct);
 router.get("/Products", getProductFavourite);
-router.delete("/DeleteProduct/:id", VerifyAuth, Decentralization(["admin"]), deleteProduct);
-router.put("/UpdateProduct/:id", VerifyAuth, Decentralization(["admin"]), uploadCloud.single("ImageUrl"), updateProduct);
+router.delete("/DeleteProduct/:id", Decentralization(["admin"]), deleteProduct);
+router.put("/UpdateProduct/:id", Decentralization(["admin"]), uploadCloud.single("ImageUrl"), updateProduct);
 router.get("/GetProductFilter", getProductFilter);
 router.get("/SearchProducts", SearchProducts);
 router.get("/productBestSeller", productBestSeller);
-router.get("/newProduct", VerifyAuth, Decentralization(["user", "admin"]), newProduct);
-router.get("/GetAllProduct", VerifyAuth, Decentralization(["admin"]), getProduct);
+router.get("/newProduct", Decentralization(["user", "admin"]), newProduct);
+router.get("/GetAllProduct", Decentralization(["admin"]), getProduct);
 // router.get("/selectTypeProduct/:type", selectTypeProduct);
 
 // Category Route
 router.get("/GetAllCategory", GetAllCategory);
-router.post("/CreateCategory", VerifyAuth, Decentralization(["admin"]), AddCategory);
-router.delete("/DeleteCategory/:id", VerifyAuth, Decentralization(["admin"]), DeleteCategory);
-router.put("/UpdateCategory/:id", VerifyAuth, Decentralization(["admin"]), UpdateCategory);
+router.post("/CreateCategory", Decentralization(["admin"]), AddCategory);
+router.delete("/DeleteCategory/:id", Decentralization(["admin"]), DeleteCategory);
+router.put("/UpdateCategory/:id", Decentralization(["admin"]), UpdateCategory);
 
 // Product Variants Route
-router.post("/CreateProductVariants", VerifyAuth, Decentralization(["admin"]), uploadCloud.single("Image"), createProductVariants);
+router.post("/CreateProductVariants", Decentralization(["admin"]), uploadCloud.single("Image"), createProductVariants);
 router.get("/GetProductVariantsByid/:id", getProductVariantsByid);
-router.get("/GetAllProductVariants", VerifyAuth, Decentralization(["admin"]), GetAllProductVariants);
+router.get("/GetAllProductVariants", Decentralization(["admin"]), GetAllProductVariants);
 // router.get("/GetStockVariants", getStockVariant);
-router.delete("/DeleteProductVariants/:id", VerifyAuth, Decentralization(["admin"]), deleteProductVariants);
-router.put("/UpdateProductVariants/:id", VerifyAuth, Decentralization(["admin"]), uploadCloud.single("Image"), updateProductVariants);
-// router.get("/GetAllProductVariants", VerifyAuth,Decentralization(["admin"]), GetAllProductVariants);
-router.put("/updateStockProduct/:id", VerifyAuth, Decentralization(["admin"]), updateStockProduct);
+router.delete("/DeleteProductVariants/:id", Decentralization(["admin"]), deleteProductVariants);
+router.put("/UpdateProductVariants/:id", Decentralization(["admin"]), uploadCloud.single("Image"), updateProductVariants);
+// router.get("/GetAllProductVariants",Decentralization(["admin"]), GetAllProductVariants);
+router.put("/updateStockProduct/:id", Decentralization(["admin"]), updateStockProduct);
 
 // Brand Route Admin
 router.get("/GetAllBrand", GetAllBrand);
-router.post("/CreateBrand", VerifyAuth, Decentralization(["admin"]), AddBrand);
-router.delete("/DeleteBrand/:id", VerifyAuth, Decentralization(["admin"]), DeleteBrand);
-router.put("/UpdateBrand/:id", VerifyAuth, Decentralization(["admin"]), UpdateBrand);
+router.post("/CreateBrand", Decentralization(["admin"]), AddBrand);
+router.delete("/DeleteBrand/:id", Decentralization(["admin"]), DeleteBrand);
+router.put("/UpdateBrand/:id", Decentralization(["admin"]), UpdateBrand);
 
 // Auth Route
 router.post("/Register", Register);
@@ -98,48 +98,48 @@ router.get("/createAccessToken", createAccessToken);
 router.get("/createRefreshToken", createRefreshToken);
 router.post("/Logout", Logout);
 router.get("/getProfileUser/:id", getProfileUser);
-router.put("/updateProfile/:id", VerifyAuth, Decentralization(["user", "admin"]), uploadCloud.single("Image"), updateProfile);
+router.put("/updateProfile/:id", Decentralization(["user", "admin"]), uploadCloud.single("Image"), updateProfile);
 
 // detail product route
 router.get("/GetDetailProduct/:id", GetDetailProduct);
 
 // Cart Route
-router.post("/AddCart/:idUser", VerifyAuth, Decentralization(["user", "admin"]), AddCart);
-router.post("/GetCart/:idUser", VerifyAuth, Decentralization(["user", "admin"]), GetCartByUser);
-router.post("/Previous", VerifyAuth, Decentralization(["user", "admin"]), handlePrevious);
-router.post("/Next", VerifyAuth, Decentralization(["user", "admin"]), handleNext);
-router.post("/DeleteCartItem", VerifyAuth, Decentralization(["user", "admin"]), handleDlCartItem);
+router.post("/AddCart/:idUser", Decentralization(["user", "admin"]), AddCart);
+router.post("/GetCart/:idUser", Decentralization(["user", "admin"]), GetCartByUser);
+router.post("/Previous", Decentralization(["user", "admin"]), handlePrevious);
+router.post("/Next", Decentralization(["user", "admin"]), handleNext);
+router.post("/DeleteCartItem", Decentralization(["user", "admin"]), handleDlCartItem);
 
 // Order Route
-router.get("/GetOrder/:idUser", VerifyAuth, Decentralization(["user", "admin"]), getOrder);
-router.get("/GetAllOrder", VerifyAuth, Decentralization(["admin", "user"]), GetAllOrder);
-router.post("/CreateOrder", VerifyAuth, Decentralization(["user", "admin"]), CreateOrder);
-router.post("/GetOrderItems/:Id_User", VerifyAuth, Decentralization(["user", "admin"]), getOrderItems);
-router.get("/GetAllOrderItems", VerifyAuth, Decentralization(["admin", "user"]), GetAllOrderItems);
-router.put("/UpdateStatusOrder/:id", VerifyAuth, Decentralization(["admin", "user"]), updateStatusOrder);
-router.post("/OrderPaymentZalo", VerifyAuth, Decentralization(["user", "admin"]), paymentWithZalopay);
-router.post("/CallbackOrder", VerifyAuth, Decentralization(["user", "admin"]), Callback);
-router.get("/GetAddressOrder/:Id_User", VerifyAuth, Decentralization(["user", "admin"]), getAddressOrder);
+router.get("/GetOrder/:idUser", Decentralization(["user", "admin"]), getOrder);
+router.get("/GetAllOrder", Decentralization(["admin", "user"]), GetAllOrder);
+router.post("/CreateOrder", Decentralization(["user", "admin"]), CreateOrder);
+router.post("/GetOrderItems/:Id_User", Decentralization(["user", "admin"]), getOrderItems);
+router.get("/GetAllOrderItems", Decentralization(["admin", "user"]), GetAllOrderItems);
+router.put("/UpdateStatusOrder/:id", Decentralization(["admin", "user"]), updateStatusOrder);
+router.post("/OrderPaymentZalo", Decentralization(["user", "admin"]), paymentWithZalopay);
+router.post("/CallbackOrder", Decentralization(["user", "admin"]), Callback);
+router.get("/GetAddressOrder/:Id_User", Decentralization(["user", "admin"]), getAddressOrder);
 
-router.post("/sendImageComment", VerifyAuth, Decentralization(["user", "admin"]), uploadCloud.array("Images"), sendImageComment);
+router.post("/sendImageComment", Decentralization(["user", "admin"]), uploadCloud.array("Images"), sendImageComment);
 router.get("/getReviewsById/:idProduct", getReviewsById);
 
-router.post("/CreateCommentLike", VerifyAuth, Decentralization(["user", "admin"]), CreateCommentLike);
-router.get("/getLikeComment/:UserId", VerifyAuth, Decentralization(["user", "admin"]), getLikeComment);
+router.post("/CreateCommentLike", Decentralization(["user", "admin"]), CreateCommentLike);
+router.get("/getLikeComment/:UserId", Decentralization(["user", "admin"]), getLikeComment);
 
 // cancel order request
-router.post("/requestCancle", VerifyAuth, Decentralization(["user", "admin"]), requestCancle);
-router.get("/getAllCancleRequests", VerifyAuth, Decentralization(["admin"]), getAllCancleRequests);
-router.put("/updateStatusCancleRequest/:id", VerifyAuth, Decentralization(["admin"]), updateStatusCancleRequest);
+router.post("/requestCancle", Decentralization(["user", "admin"]), requestCancle);
+router.get("/getAllCancleRequests", Decentralization(["admin"]), getAllCancleRequests);
+router.put("/updateStatusCancleRequest/:id", Decentralization(["admin"]), updateStatusCancleRequest);
 // chat Ai
 router.post("/chat", handleChat);
 
 // Voucher Route
-router.post("/CreateVoucher", VerifyAuth, Decentralization(["admin"]), CreateVoucher);
-router.post("/checkVoucher", VerifyAuth, Decentralization(["user", "admin"]), checkVoucher);
+router.post("/CreateVoucher", Decentralization(["admin"]), CreateVoucher);
+router.post("/checkVoucher", Decentralization(["user", "admin"]), checkVoucher);
 // Notification Route
 router.get("/getNotificationById/:id", getNotificationById);
-router.post("/markAsRead/:id", VerifyAuth, Decentralization(["user", "admin"]), markAsRead);
+router.post("/markAsRead/:id", Decentralization(["user", "admin"]), markAsRead);
 
 router.get("/getProvinces", getProvinces);
 router.get("/getDistricts/:code", getDistricts);
